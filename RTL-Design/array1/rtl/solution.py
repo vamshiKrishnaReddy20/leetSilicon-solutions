550d415869f27df3ba2879ce874be9a760eb29c2
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2026-04-01 00:41:54 +0530

    [LeetSilicon] array1: 1. Maximum and Minimum in an Array (python)
    Tests: 6 passed
    
    Auto-synced by SiliconHub

---
+++ RTL-Design/array1/rtl/solution.py
@@ -0,0 +1,12 @@
+
+class Solution:
+  def maxMin(self, nums: list[int]) -> tuple[int, int]:
+    maxi = nums[0]
+    mini = nums[0]
+    for num in nums:
+      if num > maxi :
+        maxi = num
+      elif num < mini :
+        mini = num
+
+    return (maxi, mini)
