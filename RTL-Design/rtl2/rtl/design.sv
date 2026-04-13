14f702eba12846d087ccc7b455fa7a8ba5504a94
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2026-04-14 00:06:27 +0530

    [LeetSilicon] rtl2: 30. Detect a One-Cycle High Pulse (0→1→0) (systemverilog)
    Tests: 3 passed
    
    Auto-synced by SiliconHub

--- RTL-Design/rtl2/rtl/design.sv
+++ RTL-Design/rtl2/rtl/design.sv
@@ -4,24 +4,24 @@
 // Goal: detect exactly-one-cycle high pulse using 2-cycle history. 
 
 module one_cycle_pulse (
-  input  logic clk,
-  input  logic rst_n,
-  input  logic sig_in,
-  output logic onecycle_pulse
+    input  logic clk,
+    input  logic rst_n,
+    input  logic sig_in,
+    output logic onecycle_pulse
 );
 
-
   logic d1, d2;
-  always_ff @(posedge clk or negedge rst_n) begin
-      if (!rst_n) begin
-          d1 <= 1'b0;
-          d2 <= 1'b0;
-      end else begin
-          d1 <= sig_in;
-          d2 <= d1;
-      end
-  end
 
-  assign onecycle_pulse = (d2 == 1'b0) && (d1 == 1'b1) && (sig_in == 1'b0);
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      d1 <= 1'b0;
+      d2 <= 1'b0;
+      onecycle_pulse <= 1'b0;
+    end else begin
+        d1 <= sig_in;
+        d2 <= d1;
+        onecycle_pulse <= (sig_in == 1'b0) && (d1 == 1'b1) && (d2 == 1'b0);
+    end
+  end
 
 endmodule
