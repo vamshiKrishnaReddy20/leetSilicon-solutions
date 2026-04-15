434d9cd4507ab667f15e3b27462bb3db830c2950
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2026-04-15 23:57:44 +0530

    [LeetSilicon] array6: 6. Missing Number (python)
    Auto-synced by SiliconHub

--- Programming/array6/solution.py
+++ Programming/array6/solution.py
@@ -1,9 +1,9 @@
 class Solution:
   def missingNumber(self, nums: list[int]) -> int:
-    mini = nums[0]
-    index = 0
-    for i, num in enumerate(nums, 1):
-      if num < mini:
-        mini = num
-        index = i
-    return index
+    n = len(nums)
+    total = n*(n+1) / 2
+    Tsum = sum(nums)
+    if total == Tsum:
+      return 0
+    else:
+      return int(total - Tsum)
